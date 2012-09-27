f61ed01fe7f6fde6dfa366b8e383a12377c94842
tie23 <kyle.geib@gmail.com>
2012-09-27 13:13:33 -0700

    Added the first function prototypes. Nothing more serious than that.

--- Shell.java
+++ Shell.java
@@ -1,3 +1,10 @@
 // This is still empty! :)
 import java.util.StringTokenizer;
 // Oh wait, there's something! Wait!
+
+public class Shell extends Thread {
+	public void run() {
+		// This is where I write more code!
+		// Wait. I staged but now I just added this! What do?
+	}
+}
