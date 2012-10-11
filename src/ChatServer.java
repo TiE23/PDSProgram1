0612a5f135178ff8129b454fc355c580f2b948e3
tie23 <kyle.geib@gmail.com>
2012-10-11 11:33:05 -0700

    Renamed classes and put in imports. Otherwise blank.

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,8 +1,9 @@
+package src;
 // Kyle Geib - CSS434 - Dr Fukuda - Program 01 - October 16th, 2012
 
 import java.net.*;
 import java.io.*;
 
-public class Connection {
+public class ChatServer {
 
 }
